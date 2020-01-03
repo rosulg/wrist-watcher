2408432da7f9023475f8165d53c458fdd8c0496c
Robin Sulg <robinsulg@live.com>
2020-01-03 18:35:18 +0200

    format

--- src/app/ui/ui-sidebar-left/ui-sidebar-left.component.ts
+++ src/app/ui/ui-sidebar-left/ui-sidebar-left.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { EngineService } from 'src/app/engine/engine.service';
+import {Component} from '@angular/core';
 import {SidebarNotificationService} from '../../services/sidebar-notification.service';
-import { SliderUpdaterService, Sliders } from 'src/app/services/slider-updater.service';
-import { Subscription } from 'rxjs';
+import {Sliders, SliderUpdaterService} from 'src/app/services/slider-updater.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-ui-sidebar-left',
@@ -12,7 +11,7 @@ import { Subscription } from 'rxjs';
 export class UiSidebarLeftComponent {
 
   private rotate = false;
-  private viewPosition = "none";
+  private viewPosition = 'none';
   private x_hand_rotation = 0;
   private y_hand_rotation = 0;
   private z_hand_rotation = 0;
@@ -20,69 +19,132 @@ export class UiSidebarLeftComponent {
   private zoom = 5;
   private sliders: Sliders;
   private sliderSubscription: Subscription;
-  
 
-
-onClick(item: {id: number|string, name: string, icon: string, index: number}) {
-  var x = document.getElementById("handSliders");
-  var y = document.getElementById("zoomslider");
-  x.style.display = "none";
-  y.style.display = "none";
-
-  if (item !=null) {
-    if (item.id === '2') {
-      x.style.display = "block";
-    } else if (item.id === '3') {
-      y.style.display = "block";
-    } else if (item.id === '5') {
-      this.viewPosition = "top";
-      this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
-    } else if (item.id === '6') {
-      this.viewPosition = "left";
-      this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
-    } else if (item.id === '7') {
-      this.viewPosition = "right";
-      this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
-    } else if (item.id === '8') {
-      this.rotate = !this.rotate;
-      this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
-    }
-    this.viewPosition = "none";
-  }
-  }
-
-  constructor(private sidebarNotificationService: SidebarNotificationService, private sliderUpdaterService: SliderUpdaterService) { 
+  constructor(private sidebarNotificationService: SidebarNotificationService, private sliderUpdaterService: SliderUpdaterService) {
     this.sliderSubscription = sliderUpdaterService.observable.subscribe(res => {
       this.sliders = res;
       this.updateSliders(this.sliders);
     }, err => console.log(err));
-  } 
+  }
 
-  updateSliders(sliders: Sliders){
-    if(sliders){
-      (<HTMLInputElement>document.getElementById("myRangeZoom")).value = sliders.zoom.toString();
-      console.log(sliders.zoom)
+  onClick(item: { id: number | string, name: string, icon: string, index: number }) {
+    const x = document.getElementById('handSliders');
+    const y = document.getElementById('zoomslider');
+    x.style.display = 'none';
+    y.style.display = 'none';
+
+    if (item != null) {
+      if (item.id === '2') {
+        x.style.display = 'block';
+      } else if (item.id === '3') {
+        y.style.display = 'block';
+      } else if (item.id === '5') {
+        this.viewPosition = 'top';
+        this.sidebarNotificationService.notify({
+          viewPosition: this.viewPosition,
+          rotate: this.rotate,
+          z_hand_rotation: this.z_hand_rotation,
+          x_hand_rotation: this.x_hand_rotation,
+          y_hand_rotation: this.y_hand_rotation,
+          did_zoom: this.did_zoom,
+          zoom: this.zoom
+        });
+      } else if (item.id === '6') {
+        this.viewPosition = 'left';
+        this.sidebarNotificationService.notify({
+          viewPosition: this.viewPosition,
+          rotate: this.rotate,
+          z_hand_rotation: this.z_hand_rotation,
+          x_hand_rotation: this.x_hand_rotation,
+          y_hand_rotation: this.y_hand_rotation,
+          did_zoom: this.did_zoom,
+          zoom: this.zoom
+        });
+      } else if (item.id === '7') {
+        this.viewPosition = 'right';
+        this.sidebarNotificationService.notify({
+          viewPosition: this.viewPosition,
+          rotate: this.rotate,
+          z_hand_rotation: this.z_hand_rotation,
+          x_hand_rotation: this.x_hand_rotation,
+          y_hand_rotation: this.y_hand_rotation,
+          did_zoom: this.did_zoom,
+          zoom: this.zoom
+        });
+      } else if (item.id === '8') {
+        this.rotate = !this.rotate;
+        this.sidebarNotificationService.notify({
+          viewPosition: this.viewPosition,
+          rotate: this.rotate,
+          z_hand_rotation: this.z_hand_rotation,
+          x_hand_rotation: this.x_hand_rotation,
+          y_hand_rotation: this.y_hand_rotation,
+          did_zoom: this.did_zoom,
+          zoom: this.zoom
+        });
+      }
+      this.viewPosition = 'none';
     }
   }
-  passHandRotationX(value){
+
+  updateSliders(sliders: Sliders) {
+    if (sliders) {
+      (<HTMLInputElement>document.getElementById('myRangeZoom')).value = sliders.zoom.toString();
+      console.log(sliders.zoom);
+    }
+  }
+
+  passHandRotationX(value) {
     this.x_hand_rotation = value;
-    this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
+    this.sidebarNotificationService.notify({
+      viewPosition: this.viewPosition,
+      rotate: this.rotate,
+      z_hand_rotation: this.z_hand_rotation,
+      x_hand_rotation: this.x_hand_rotation,
+      y_hand_rotation: this.y_hand_rotation,
+      did_zoom: this.did_zoom,
+      zoom: this.zoom
+    });
   }
 
-  passHandRotationY(value){
+  passHandRotationY(value) {
     this.y_hand_rotation = value;
-    this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
+    this.sidebarNotificationService.notify({
+      viewPosition: this.viewPosition,
+      rotate: this.rotate,
+      z_hand_rotation: this.z_hand_rotation,
+      x_hand_rotation: this.x_hand_rotation,
+      y_hand_rotation: this.y_hand_rotation,
+      did_zoom: this.did_zoom,
+      zoom: this.zoom
+    });
   }
 
-  passHandRotationZ(value){
+  passHandRotationZ(value) {
     this.z_hand_rotation = value;
-    this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
+    this.sidebarNotificationService.notify({
+      viewPosition: this.viewPosition,
+      rotate: this.rotate,
+      z_hand_rotation: this.z_hand_rotation,
+      x_hand_rotation: this.x_hand_rotation,
+      y_hand_rotation: this.y_hand_rotation,
+      did_zoom: this.did_zoom,
+      zoom: this.zoom
+    });
   }
 
   cameraZoom(value) {
     this.zoom = value;
     this.did_zoom = true;
-    this.sidebarNotificationService.notify({viewPosition: this.viewPosition, rotate: this.rotate,  z_hand_rotation: this.z_hand_rotation, x_hand_rotation: this.x_hand_rotation, y_hand_rotation: this.y_hand_rotation, did_zoom: this.did_zoom, zoom: this.zoom});
+    this.sidebarNotificationService.notify({
+      viewPosition: this.viewPosition,
+      rotate: this.rotate,
+      z_hand_rotation: this.z_hand_rotation,
+      x_hand_rotation: this.x_hand_rotation,
+      y_hand_rotation: this.y_hand_rotation,
+      did_zoom: this.did_zoom,
+      zoom: this.zoom
+    });
     this.did_zoom = false;
   }
 }
