eb98b50b11c059ca0ddff246692f80e569f628dc
Robin Sulg <robinsulg@live.com>
2020-01-03 19:00:22 +0200

    make matslider draggable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { EngineComponent } from './engine/engine.component';
-import { UiInfobarBottomComponent } from './ui/ui-infobar-bottom/ui-infobar-bottom.component';
-import { UiInfobarTopComponent } from './ui/ui-infobar-top/ui-infobar-top.component';
-import { UiSidebarLeftComponent } from './ui/ui-sidebar-left/ui-sidebar-left.component';
-import { UiSidebarRightComponent } from './ui/ui-sidebar-right/ui-sidebar-right.component';
-import { UiComponent } from './ui/ui.component';
-import { SliderComponent } from './ui/slider/slider.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule, MatSidenavModule, MatListModule } from '@angular/material';
+import 'hammerjs';
+import {NgModule} from '@angular/core';
+import {BrowserModule, HAMMER_GESTURE_CONFIG} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {EngineComponent} from './engine/engine.component';
+import {UiInfobarBottomComponent} from './ui/ui-infobar-bottom/ui-infobar-bottom.component';
+import {UiInfobarTopComponent} from './ui/ui-infobar-top/ui-infobar-top.component';
+import {UiSidebarLeftComponent} from './ui/ui-sidebar-left/ui-sidebar-left.component';
+import {UiSidebarRightComponent} from './ui/ui-sidebar-right/ui-sidebar-right.component';
+import {UiComponent} from './ui/ui.component';
+import {SliderComponent} from './ui/slider/slider.component';
+import {GestureConfig, MatListModule, MatSidenavModule, MatSliderModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -27,12 +28,17 @@ import { MatSliderModule, MatSidenavModule, MatListModule } from '@angular/mater
     BrowserAnimationsModule,
     MatSliderModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
   ],
   providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: GestureConfig,
+    }
   ],
   bootstrap: [
     AppComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+}
